5d1ec25f141070b464ae6174c710a28d9e391875
Reikyuu09 <muflihalrasyid@gmail.com>
2025-06-19 13:03:12 +0700

    migrasi

--- database/migrations/2025_06_19_053400_create_lamarans_table.php
+++ database/migrations/2025_06_19_053400_create_lamarans_table.php
@@ -11,9 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('lamaran', function (Blueprint $table) {
+        Schema::create('lamarans', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table ->string('nama');
         });
     }
 
@@ -22,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('lamaran');
+        Schema::dropIfExists('lamarans');
     }
 };
